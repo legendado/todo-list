c899288ebfa675f5a54ea7b9eab47dabee7df9dd
Alexey Dmitriev <legendado70@gmail.com>
2019-03-27 11:29:52 +0200

    DELETE method with exceptions

---
+++ server/app/Services/AuthorizationService.js
@@ -0,0 +1,16 @@
+const InvalidAccessException = use('App/Exceptions/InvalidAccessException')
+const ResourceNotExistException = use('App/Exceptions/ResourceNotExistException')
+
+class AuthorizathionService {
+    verifyPermission(resource, user) {
+        if(!resource) {
+            throw new ResourceNotExistException()
+        }
+
+        if (resource.user_id !== user.id) {
+            throw new InvalidAccessException()
+        }
+    }
+}
+
+module.exports = new AuthorizathionService()
